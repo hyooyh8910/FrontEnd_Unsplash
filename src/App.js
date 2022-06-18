81f09b7404bffe5a4a9ab15b312c9b4db65becdf
Maiowol <103277726+Maiowol@users.noreply.github.com>
2022-06-18 13:26:49 +0900

    Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+import { Routes, Route } from 'react-router-dom';
 
+
+//components
+import Main from "./pages/Main";
+import LogIn from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Upload from "./pages/Upload";
 
 
 function App() {
+
   return (
-    <div className="App">
-   
-    </div>
+    <>
+    <Routes>
+      <Route exact path="/posts" element={< Main/>} />
+      <Route exact path="/user/signin" element={< LogIn/>} />
+      <Route exact path="/user/signup" element={< SignUp/>} /> 
+      <Route exact path="/posts/post" element={< Upload />} />
+    </Routes>
+  </>
   );
 }
 
