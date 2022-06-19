e0a970e9b22a0c8bebe9b79697e9d77f0be45e5b
hyooyh8910 <hyooyh8910@naver.com>
2022-06-19 12:18:57 +0900

    Add configStore into index.js
    연결만 해둠. 아직 리덕스 사용 안함

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './shared/App';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+
 import reportWebVitals from './reportWebVitals';
+
+import App from './shared/App';
+import store from "./redux/configureStore";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  // </Provider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
